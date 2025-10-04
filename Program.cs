8e96e73134aa606973937bc2a37a0a0462e2dc31
charlie <Charliesamuel83@gmail.com>
2025-10-04 13:25:01 +0200

    Refaktorerade till separata metoder och använde switch för tydligare struktur och bättre underhållbarhet.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ namespace Kodanalys
     class Program
     {
         static List<string> users = new List<string>();
-        static int userCount = 0;
 
         static void Main(string[] args)
         {
             bool isRunning = true;
+
             while (isRunning)
             {
                 Console.WriteLine("Välj ett alternativ:");
@@ -19,71 +19,84 @@ namespace Kodanalys
                 Console.WriteLine("3. Ta bort användare");
                 Console.WriteLine("4. Sök användare");
                 Console.WriteLine("5. Avsluta");
+                Console.Write("Ditt val: ");
                 string choice = Console.ReadLine();
 
-                if (choice == "1")
+                switch (choice)
                 {
-                    Console.Write("Ange namn: ");
-                    string name = Console.ReadLine();
+                    case "1":
+                        AddUser();
+                        break;
+                    case "2":
+                        ShowUsers();
+                        break;
+                    case "3":
+                        RemoveUser();
+                        break;
+                    case "4":
+                        SearchUser();
+                        break;
+                    case "5":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val.");
+                        break;
+                }
 
-                    if (users.Count < 10)
-                    {
-                        users.Add(name);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Listan är full!");
-                    }
-                }
-                else if (choice == "2")
-                {
-                    Console.WriteLine("Användare:");
-                    foreach (var user in users)
-                    {
-                        Console.WriteLine(user);
-                    }
-                }
-                else if (choice == "3")
-                {
-                    Console.Write("Ange namn att ta bort: ");
-                    string nameToRemove = Console.ReadLine();
-
-                    if (users.Remove(nameToRemove))
-                    {
-                        Console.WriteLine("Användare borttagen.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Användaren hittades inte.");
-                    }
-                }
-                
-                  
-                else if (choice == "4")
-                {
-                    Console.Write("Ange namn att söka: ");
-                    string query = Console.ReadLine();
-
-                    if (users.Contains(query))
-                    {
-                        Console.WriteLine("Användaren finns i listan.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Användaren hittades inte.");
-                    }
-
-                }
-                else if (choice == "5")
-                {
-                    isRunning = false;
-                }
-                else
-                {
-                    Console.WriteLine("Ogiltigt val.");
-                }
                 Console.WriteLine();
             }
+        }
+
+        static void AddUser()
+        {
+            Console.Write("Ange namn: ");
+            string name = Console.ReadLine();
+
+            if (users.Count < 10)
+            {
+                users.Add(name);
+                Console.WriteLine("Användare tillagd.");
+            }
+            else
+            {
+                Console.WriteLine("Listan är full!");
+            }
+        }
+
+        static void ShowUsers()
+        {
+            if (users.Count == 0)
+            {
+                Console.WriteLine("Inga användare finns.");
+                return;
+            }
+
+            Console.WriteLine("Användare:");
+            foreach (var user in users)
+                Console.WriteLine(user);
+        }
+
+        static void RemoveUser()
+        {
+            Console.Write("Ange namn att ta bort: ");
+            string nameToRemove = Console.ReadLine();
+
+            if (users.Remove(nameToRemove))
+                Console.WriteLine("Användare borttagen.");
+            else
+                Console.WriteLine("Användaren hittades inte.");
+        }
+
+        static void SearchUser()
+        {
+            Console.Write("Ange namn att söka: ");
+            string query = Console.ReadLine();
+
+            if (users.Contains(query))
+                Console.WriteLine("Användaren finns i listan.");
+            else
+                Console.WriteLine("Användaren hittades inte.");
         }
     }
 }
